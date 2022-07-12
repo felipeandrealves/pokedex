3673411657490a9da1070db49081b9123a90607e
Felipe Andre Alves <felipe_alves_sbo@hotmail.com>
2022-07-12 12:11:41 -0300

    fix: vertical scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
+import "../styles/global.css";
+
 import { theme } from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
